361462f9dfd93445c7ce1df2bd1d8e585179fd71
the-code-machine <sarthakkhare821@gmail.com>
2026-02-09 01:03:03 +0530

    update

--- app/scheduler.py
+++ app/scheduler.py
@@ -8,7 +8,7 @@ def start_scheduler():
     scheduler.add_job(
         run,
         trigger="interval",
-        minutes=2,          # 👈 runs every 2 minutes
+        minutes=60,          # 👈 runs every 2 minutes
         id="youtube_worker",
         replace_existing=True,
         max_instances=1,    # prevents overlapping jobs
