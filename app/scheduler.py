cdc6e682eb6f0425abc141ce8c15e68a7870351e
the-code-machine <coboxgames@gmail.com>
2026-03-26 17:09:50 +0530

    update

--- app/scheduler.py
+++ app/scheduler.py
@@ -24,7 +24,7 @@ def start_scheduler():
     scheduler.add_job(
         run_youtube, 
         "interval", 
-        hours=8, 
+        hours=2, 
         id="youtube_discovery",
         max_instances=1,
         replace_existing=True
